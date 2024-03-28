c15d53fe8e9db5d82249c36ff4c337558dce2b53
Yamii <137888032+LittleYamii@users.noreply.github.com>
2024-03-28 09:41:37 +0100

    critere

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import SecondPart from './components/secondpart/SecondPart'
+import Critere from './components/firstpart/Critere'
 function App() {
   return (
     <div>
+      <Critere/>
       <SecondPart/> 
     </div>
   )
